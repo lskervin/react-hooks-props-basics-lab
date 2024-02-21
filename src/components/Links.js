70564e333da8d527b00ad2130994318ce69793b4
LeightonSkervin <lskervin23@gmail.com>
2024-02-21 09:06:06 -0500

    lab complete

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+function Links(links) {
+     return(
+        <div>
+        <h3>Links</h3>
+        <a href={links.github}>{links.github}</a>
+        <a href={links.linkedin}>{links.linkedin}</a>
+        </div>
+     );
+}
+
+export default Links;   
